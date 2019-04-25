6723940955d8d391756c648908afe2b4ac52cccc
mongsoup <49330609+mongsoup@users.noreply.github.com>
2019-04-25 10:25:00 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-/*메인 롤링 영역 탭메뉴*/
+/*section*/
 var oldTab = "main-section-1";
 var oldMem = "mem-01";
 
@@ -40,29 +40,20 @@ obj.style.display = "block"
 
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
- 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
- 
-// Get the <span> element that closes the modal
-var spanx = document.getElementsByClassName("close")[0];                                          
- 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-            modal.style.display = "block";
+function chgIndex1() {
+	document.getElementById('box-1r').style.zIndex = 1;
+	document.getElementById('box-2r').style.zIndex = -1;
+	document.getElementById('box-3r').style.zIndex = -1;
 }
- 
-// When the user clicks on <span> (x), close the modal
-spanx.onclick = function() {
-            modal.style.display = "none";
- }
- 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
- }
-  
+
+function chgIndex2() {
+	document.getElementById('box-1r').style.zIndex = -1;
+	document.getElementById('box-2r').style.zIndex = 1;
+	document.getElementById('box-3r').style.zIndex = -1;
+}
+
+function chgIndex3() {
+	document.getElementById('box-1r').style.zIndex = -1;
+	document.getElementById('box-2r').style.zIndex = -1;
+	document.getElementById('box-3r').style.zIndex = 1;
+}
